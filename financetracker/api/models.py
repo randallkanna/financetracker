aa8b8bc00ec115a69bc282b07f51c4ac81126c66
randallkanna <randallkanna@gmail.comm>
2017-07-28 19:51:03 -0700

    adds user tests

--- financetracker/api/models.py
+++ financetracker/api/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class List(models.Model):
     name = models.CharField(max_length=255, blank=False, unique=True)
+    owner = models.ForeignKey('auth.User',
+                              related_name='lists',
+                              on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
--- financetracker/api/tests.py
+++ financetracker/api/tests.py
@@ -3,11 +3,13 @@ from .models import List
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 class ModelTestCase(TestCase):
     def setUp(self):
+        user = User.objects.create(username="test")
         self.list_name = "Test"
-        self.list = List(name=self.list_name)
+        self.list = List(name=self.list_name, owner=user)
 
     def test_model_can_create_a_list(self):
         old_count = List.objects.count()
@@ -17,8 +19,10 @@ class ModelTestCase(TestCase):
 
 class ViewTestCase(TestCase):
     def setUp(self):
+        user = User.objects.create(username="Test")
         self.client = APIClient()
-        self.list_data = {'name': 'Test'}
+        self.client.force_authenticate(user=user)
+        self.list_data = {'name': 'Test', 'owner': user.id}
         self.response = self.client.post(
             reverse('create'),
             self.list_data,
@@ -27,33 +31,35 @@ class ViewTestCase(TestCase):
     def test_api_can_create_a_list(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_authorization_is_enforced(self):
+        new_client = APIClient()
+        res = new_client.get('/lists', kwargs={'pk': 3}, format="json")
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_api_can_get_a_list(self):
-        list = List.objects.get()
+        list = List.objects.get(id=1)
         response = self.client.get(
-            reverse('details'),
-            kwargs={'pk': list.id}, format="json")
+            '/bucketlists/',
+            kwargs={'pk': bucketlist.id}, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertContains(response, list)
 
-    def test_api_can_update_list(self):
-        change_list = {'name': 'newName'}
-
+    def test_api_can_update_a_list(self):
+        list = List.objects.get()
+        change_list = {'name': 'new'}
         res = self.client.put(
             reverse('details', kwargs={'pk': list.id}),
             change_list, format='json'
         )
-
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
 
-    def test_api_can_delete_list(self):
+    def test_api_can_delete_a_list(self):
         list = List.objects.get()
-
-        response = self.client.dete(
-            reverse('details', kwargs={'pk: list.id'}),
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': list.id}),
             format='json',
             follow=True
         )
-
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
