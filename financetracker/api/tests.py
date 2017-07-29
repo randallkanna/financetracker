d478564869aad281012aad960ed25b9fb9b7d55f
randallkanna <randallkanna@gmail.comm>
2017-07-28 19:28:03 -0700

    adds lists api

--- financetracker/api/tests.py
+++ financetracker/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import List
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 
 class ModelTestCase(TestCase):
     def setUp(self):
@@ -11,3 +14,16 @@ class ModelTestCase(TestCase):
         self.list.save()
         new_count = List.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.list_data = {'name': 'Test'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.list_data,
+            format="json")
+
+    def test_api_can_create_a_list(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
--- financetracker/api/views.py
+++ financetracker/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ListSerializer
+from .models import List
 
-# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
